3a396e5841327c424732cea2577c62bfe02aac8b
Hrant Karapetyan <amigo.club@mail.ru>
2020-04-21 14:50:04 +0400

    registration page XML is ready on 99,99%

--- app/src/main/java/com/hashdroid/am/users/login/UsersLogin.java
+++ app/src/main/java/com/hashdroid/am/users/login/UsersLogin.java
@@ -1,16 +1,30 @@
 package com.hashdroid.am.users.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hashdroid.am.R;
+import com.hashdroid.am.users.register.UsersRegister;
 
 public class UsersLogin extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        final Intent intent = new Intent(this, UsersRegister.class);
+
+        TextView registerBtn = findViewById(R.id.login_sign_up_text);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/hashdroid/am/users/register/UsersRegister.java
+++ app/src/main/java/com/hashdroid/am/users/register/UsersRegister.java
@@ -1,15 +1,11 @@
 package com.hashdroid.am.users.register;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hashdroid.am.R;
-import com.hashdroid.am.users.login.UsersLogin;
 
 public class UsersRegister extends AppCompatActivity {
     @Override
@@ -17,14 +13,5 @@ public class UsersRegister extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
 
-        final Intent intent = new Intent(this, UsersLogin.class);
-
-        Button registerBtn = findViewById(R.id.registerBtn);
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(intent);
-            }
-        });
     }
 }
